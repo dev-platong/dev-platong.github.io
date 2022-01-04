2ec740de6287f83bce98028d5ce427e4ea42bcef
dev-platong <platong8249@gmail.com>
2022-01-05 04:17:22 +0900

    Initial playback with ShakaPlayer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+	import setup from '../scripts/player';
+	setup();
+</script>
+
 <svelte:head>
 	<title>dev-platong.github.io</title>
 </svelte:head>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<video id="shaka_player" controls />
